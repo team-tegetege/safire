e4f2fcf3f7cd40ac1d6510f7707ebea6c203886f
yuta.kawakami <kami.981108@gmail.com>
2021-09-20 09:45:48 +0900

    Fixed home page getter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,19 @@
 class HomeController < ApplicationController
   def index
-    limit = 6 unless home_params["limit"].present?
-    offset = 0 unless home_params["offset"].present?
+    limit = home_params["limit"].present? ? home_params["limit"] : 6
+    offset = home_params["offset"].present? ? home_params["offset"] : 0
 
     # 他ユーザのプロジェクトリスト
-    projects = Project.order(created_at: "DESC")
+    projects = Project.order(created_at: "DESC").limit(limit).offset(offset)
     projects_list = []
     projects.each do |project|
       # タグを探して
       tags = ProjectTag.where(project_id: project.id)
       # くっつけて
       one_object = project.attributes.merge({ tag_list: tags.map(&:tag) })
+      # filter
+      object_list = ["id", "user_id", "title", "created_at", "thumbnail", :tag_list]
+      one_object = one_object.select {|k,v| object_list.include?(k)}
       # push
       projects_list.push(one_object)
     end
